d78c5c3d00406f76c4b1f6fcdbca8a107d12731b
andre mcmorris <ajmcmorris@gmail.com>
2024-09-29 07:49:51 -0400

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/components/home/home.component';
+import { RegisterComponent } from './core/components/register/register.component';
+import { LoginComponent } from './core/components/login/login.component';
 import { ChatComponent } from './features/chat/chat.component';
 
 export const routes: Routes = [
   {
-    component: ChatComponent,
+    component: HomeComponent,
     path: '',
+  },
+  {
+    component: RegisterComponent,
+    path: 'register',
+  },
+  {
+    component: LoginComponent,
+    path: 'login',
+  },
+  {
+    component: ChatComponent,
+    path: 'chat',
   },
 ];
